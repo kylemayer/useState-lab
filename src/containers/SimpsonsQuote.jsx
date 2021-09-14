cbb0bf084a1e6db3a4ee4d9c57d114c197941751
Kyle Mayer <x@kylemayer.dev>
2021-09-14 10:59:38 -0700

    container and test for container setup

--- src/containers/SimpsonsQuote.jsx
+++ src/containers/SimpsonsQuote.jsx
@@ -1,1 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Quote from '../components/quote/Quote';
+import Load from '../components/quote/Load';
+import { getQuote } from '../services/simpsonsApi';
+
+const SimpsonsQuote = () => {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    const quote = await getQuote();
+    setQuote(quote);
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Load onclick={handleClick} />
+      {loading ? <p>Loading spinner</p> : quote &&
+        <Quote
+          image={quote.image}
+          character={quote.character}
+          quote={quote.quote} />}
+    </>
+  );
+};
+
+export default SimpsonsQuote;
---
+++ src/containers/SimpsonsQuote.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import SimpsonsQuote from './SimpsonsQuote';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const server = setupServer(
+  rest.get(
+    'https://thesimpsonsquoteapi.glitch.me/quotes',
+    (req, res, ctx) => {
+      return res(
+        ctx.json([
+          {
+            quote: 'Doughnuts?',
+            character: 'Mr. Burns',
+            image: 'https://example.com/mrburns.png',
+          },
+        ])
+      );
+    }
+  )
+);
+
+describe('Simpsons Quote Container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('displays a button that fetches a random quote', async () => {
+    render(<SimpsonsQuote />);
+
+    const fetchButton = screen.getByRole('button', { name: 'Get me a quote!' });
+    fireEvent.click(fetchButton);
+
+    return waitFor(() => {
+      screen.getByText('Doughnuts?');
+    });
+  });
+});
